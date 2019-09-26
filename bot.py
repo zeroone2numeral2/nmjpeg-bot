7845778f8b8a1ab5920d68473f5924f3df943ea3
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-09-26 11:31:38 +0200

    Code lints

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import importlib
+
 from telegram.ext import Updater
 from telegram.ext.dispatcher import run_async
 
@@ -8,25 +9,28 @@ updater = Updater(token=os.environ.get('TG_TOKEN') or "")
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format='[%(asctime)s][%(name)s][%(levelname)s] %(message)s',
-	level=logging.INFO)
+                    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
+
 @run_async
 def error_callback(bot, update, error):
-	pass # p-t-b's logger already logs this to the console
+    pass  # p-t-b's logger already logs this to the console
+
 
 def main():
-	for modname in ["compressor", "help"]:
-		module = getattr(importlib.import_module('modules.{}'.format(modname)), "module") # from modules.modname import module
-		logger.info("importing module: %s (handlers: %d)", module.name, len(module.handlers))
-		for handler in module.handlers:
-			dispatcher.add_handler(handler)
+    for modname in ["compressor", "help"]:
+        module = getattr(importlib.import_module('modules.{}'.format(modname)), "module")  # from modules.modname import module
+        logger.info("importing module: %s (handlers: %d)", module.name, len(module.handlers))
+        for handler in module.handlers:
+            dispatcher.add_handler(handler)
 
-	dispatcher.add_error_handler(error_callback)
+    dispatcher.add_error_handler(error_callback)
 
-	updater.start_polling(clean=True)
-	updater.idle()
+    updater.start_polling(clean=True)
+    updater.idle()
+
 
 if __name__ == '__main__':
-	main()
+    main()
--- modules/help.py
+++ modules/help.py
@@ -1,4 +1,5 @@
 import logging
+
 from telegram.ext import CommandHandler
 from telegram.ext import BaseFilter
 from telegram.ext.dispatcher import run_async
@@ -6,9 +7,11 @@ from telegram import ParseMode
 
 logger = logging.getLogger(__name__)
 
+
 class FilterPrivateChat(BaseFilter):
     def filter(self, message):
         return message.chat_id > 0
+
 
 private_chat = FilterPrivateChat()
 
@@ -23,14 +26,16 @@ Command shortcuts: `/nmjpeg, /morejpeg, /jpeg` (the "e" in "jpeg" is optional)
 [source code](https://github.com/zeroone2numeral2/nmjpeg-bot)\
 """
 
+
 @run_async
 def send_help(bot, update):
-	logger.info("start or help command")
-	bot.send_message(update.message.chat_id, help_message,
-		parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+    logger.info("start or help command")
+    bot.send_message(update.message.chat_id, help_message,
+                     parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
 
 class module:
-	name = "help"
-	handlers = (
-		CommandHandler(["start", "help"], send_help),
-	)
+    name = "help"
+    handlers = (
+        CommandHandler(["start", "help"], send_help),
+    )
